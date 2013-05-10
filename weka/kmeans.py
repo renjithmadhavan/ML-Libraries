0f12b00c9f4bc472d8d2878085818e702354ed87
Marcus Edel <marcus@virtual-artz.de>
2013-05-10 18:27:30 +0200

    add comments

--- weka/kmeans.py
+++ weka/kmeans.py
@@ -1,15 +1,18 @@
 from subprocess import Popen, PIPE, STDOUT
 
+# Settings to perform kmeans.
 classpath = '~/weka.jar'
 wekaFilter = 'weka.filters.unsupervised.attribute.AddCluster'
 methode = 'weka.clusterers.SimpleKMeans -N 2'
 inputFile = '../data/two_cluster.arff'
 
+# Perform kmeans with 2 clusters.
 p = Popen(['java', '-classpath', classpath,
 			'weka.filters.unsupervised.attribute.AddCluster', 
 			'-W', methode, '-i', inputFile],
 			shell=False, bufsize=1, stdout=PIPE, stderr=STDOUT, close_fds=True)
 
+# Show cluster association.
 for line in iter(p.stdout.readline, b''):
     print line,
 p.stdout.close()
